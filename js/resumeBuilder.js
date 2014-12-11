1cdf48676a94fe70d8a7c052fdb1077fa139e850
LaurelH <laurel@thisisaok.com>
2014-12-11 01:10:00 -0800

    Add funThoughts

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,6 @@
 $("#main").append("Laurel");
+var awesomeThoughts = "I am Laurel and I am AWESOME"
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+console.log(awesomeThoughts);
+console.log(funThoughts);
+$("#main").append(funThoughts);
